88f32aa0c4a8562bd024daf55526018dbf9dee11
Aidan Perret <aperret@me.com>
2021-07-22 10:19:17 -0400

    commit tree 04adceaa7925d1fc0f57e5a51b3ea27467fa6235

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,15 @@
 // Code EyesOnMe Component Here
+function Focus () {
+    console.log("Good!")
+}
+
+function Blur () {
+    console.log('Hey! Eyes on me!')
+}
+
+function EyesOnMe () {
+    return (
+        <button onFocus={Focus} onBlur={Blur}>Eyes on me</button>
+    )
+}
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+
+function Password() {
+    console.log("Entering password...")
+}
+
+function Keypad () {
+    return (
+        <input type="password" onChange={Password}/>
+    )
+}
+export default Keypad
